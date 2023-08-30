738b4627ce8a69712e4f0eb016e046426489c605
menishmueli <menishmueli@gmail.com>
2023-08-30 09:28:24 +0300

    add text animation

--- spark-ui/src/InfoBox.tsx
+++ spark-ui/src/InfoBox.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import { Grid, Paper } from '@mui/material';
 import PropTypes from "prop-types";
+import styles from './InfoBox.module.css'; // Import css modules stylesheet as styles
 
 type InfoBoxProps = {
   title: string,
@@ -12,6 +13,19 @@ type InfoBoxProps = {
 
 export default function InfoBox({title, text, color, icon} : InfoBoxProps) {
   const Icon = icon;
+
+  const [displayText, setDisplayText] = React.useState(text);
+  const [blink, setBlink] = React.useState(false);
+
+  React.useEffect(() => {
+    setBlink(true);
+    setDisplayText(text);
+    const timer = setTimeout(() => {
+      setBlink(false);
+    }, 500);  // Animation duration
+    return () => clearTimeout(timer);
+  }, [text]);
+
   return (
     <Grid item lg={2}>
     <Paper
@@ -27,8 +41,8 @@ export default function InfoBox({title, text, color, icon} : InfoBoxProps) {
             {title}
             <Icon sx={{ ml: 1}} />
         </Typography>
-      <Typography component="p" variant="h4" display="flex" justifyContent="center" alignItems="center">
-        {text}
+      <Typography component="p" variant="h4" display="flex" justifyContent="center" alignItems="center" className={blink ? styles.blink: ""}>
+        {displayText}
       </Typography>
       {/* <Typography color="text.secondary" sx={{ flex: 1 }}>
         {secondaryText}
