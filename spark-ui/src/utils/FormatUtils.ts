839f10a19f9105ec74a0c5362260809e4c2d0273
menishmueli <menishmueli@gmail.com>
2023-09-13 16:58:50 +0300

    fix import in format utils 2

--- spark-ui/src/utils/FormatUtils.ts
+++ spark-ui/src/utils/FormatUtils.ts
@@ -1,4 +1,4 @@
-function humanFileSize(bytes: number, si = true): string {
+export function humanFileSize(bytes: number, si = true): string {
     let thresh = si ? 1000 : 1024;
     if (bytes < thresh) return bytes + ' B';
     let units = si ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
@@ -9,5 +9,3 @@ function humanFileSize(bytes: number, si = true): string {
     } while (bytes >= thresh);
     return bytes.toFixed(1) + ' ' + units[u];
   };
-
-  export default humanFileSize
