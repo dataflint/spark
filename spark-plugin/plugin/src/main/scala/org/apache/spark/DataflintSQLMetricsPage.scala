da656ed3c725a240bd86cc7c4c37cc82d53b8e17
menishmueli <menishmueli@gmail.com>
2023-10-26 15:16:12 +0300

    add try catch for errors for SQL metrics or SQL plan

--- spark-plugin/plugin/src/main/scala/org/apache/spark/DataflintSQLMetricsPage.scala
+++ spark-plugin/plugin/src/main/scala/org/apache/spark/DataflintSQLMetricsPage.scala
@@ -1,5 +1,6 @@
 package org.apache.spark
 
+import org.apache.spark.internal.Logging
 import org.apache.spark.ui.{SparkUI, WebUIPage}
 import org.json4s.JsonAST
 
@@ -9,29 +10,37 @@ import org.apache.spark.sql.execution.ui.{SQLAppStatusListener, SQLAppStatusStor
 import org.json4s.JsonAST.JValue
 import org.json4s._
 
-class DataflintSQLMetricsPage(ui: SparkUI, sqlListener: () => Option[SQLAppStatusListener]) extends WebUIPage("sqlmetrics") {
+class DataflintSQLMetricsPage(ui: SparkUI, sqlListener: () => Option[SQLAppStatusListener])
+  extends WebUIPage("sqlmetrics") with Logging {
   private var sqlListenerCache: Option[SQLAppStatusListener] = None
 
   override def renderJson(request: HttpServletRequest): JsonAST.JValue = {
-    if(sqlListenerCache.isEmpty){
-      sqlListenerCache = sqlListener()
-    }
+    try {
+      if (sqlListenerCache.isEmpty) {
+        sqlListenerCache = sqlListener()
+      }
 
-    val sqlStore = new SQLAppStatusStore(ui.store.store, sqlListenerCache)
-    val executionId = request.getParameter("executionId")
-    if(executionId == null) {
-      return JObject()
+      val sqlStore = new SQLAppStatusStore(ui.store.store, sqlListenerCache)
+      val executionId = request.getParameter("executionId")
+      if (executionId == null) {
+        return JObject()
+      }
+      val executionIdLong = executionId.toLong
+      val metrics = sqlStore.executionMetrics(executionIdLong)
+      val graph = sqlStore.planGraph(executionIdLong)
+      val nodesMetrics = graph.allNodes.map(node => NodeMetrics(node.id, node.name, node.metrics.map(metric => {
+          NodeMetric(metric.name, metrics.get(metric.accumulatorId))
+        })))
+        // filter nodes without metrics
+        .filter(nodeMetrics => !nodeMetrics.metrics.forall(_.value.isEmpty))
+      val jValue: JValue = Extraction.decompose(nodesMetrics)(org.json4s.DefaultFormats)
+      jValue
+    } catch {
+      case e: Throwable => {
+        logError("failed to serve dataflint SQL metrics", e)
+        JObject()
+      }
     }
-    val executionIdLong = executionId.toLong
-    val metrics = sqlStore.executionMetrics(executionIdLong)
-    val graph = sqlStore.planGraph(executionIdLong)
-    val nodesMetrics = graph.allNodes.map(node => NodeMetrics(node.id, node.name, node.metrics.map(metric => {
-        NodeMetric(metric.name, metrics.get(metric.accumulatorId))
-      })))
-      // filter nodes without metrics
-      .filter(nodeMetrics => !nodeMetrics.metrics.forall(_.value.isEmpty))
-    val jValue: JValue = Extraction.decompose(nodesMetrics)(org.json4s.DefaultFormats)
-    jValue
   }
 
   override def render(request: HttpServletRequest): Seq[Node] = Seq[Node]()
--- spark-plugin/plugin/src/main/scala/org/apache/spark/DataflintSQLPlanPage.scala
+++ spark-plugin/plugin/src/main/scala/org/apache/spark/DataflintSQLPlanPage.scala
@@ -1,5 +1,6 @@
 package org.apache.spark
 
+import org.apache.spark.internal.Logging
 import org.apache.spark.ui.{SparkUI, WebUIPage}
 import org.json4s.JsonAST
 
@@ -9,27 +10,36 @@ import org.apache.spark.sql.execution.ui.{SQLAppStatusListener, SQLAppStatusStor
 import org.json4s.JsonAST.JValue
 import org.json4s._
 
-  class DataflintSQLPlanPage(ui: SparkUI, sqlListener: () => Option[SQLAppStatusListener]) extends WebUIPage("sqlplan") {
+class DataflintSQLPlanPage(ui: SparkUI, sqlListener: () => Option[SQLAppStatusListener])
+  extends WebUIPage("sqlplan") with Logging {
   private var sqlListenerCache: Option[SQLAppStatusListener] = None
 
   override def renderJson(request: HttpServletRequest): JsonAST.JValue = {
-    if(sqlListenerCache.isEmpty){
-      sqlListenerCache = sqlListener()
-    }
-    val sqlStore = new SQLAppStatusStore(ui.store.store, sqlListenerCache)
+    try {
+      if (sqlListenerCache.isEmpty) {
+        sqlListenerCache = sqlListener()
+      }
+      val sqlStore = new SQLAppStatusStore(ui.store.store, sqlListenerCache)
 
-    val offset = request.getParameter("offset")
-    val length = request.getParameter("length")
-    if (offset == null || length == null) {
-      return JArray(List())
-    }
+      val offset = request.getParameter("offset")
+      val length = request.getParameter("length")
+      if (offset == null || length == null) {
+        return JArray(List())
+      }
 
-    val sqlPlans = sqlStore.executionsList(offset.toInt, length.toInt).map { exec =>
-      val graph = sqlStore.planGraph(exec.executionId)
-      SqlEnrichedData(exec.executionId, graph.allNodes.length, graph.allNodes.map(node => NodePlan(node.id, node.desc)))
+      val sqlPlans = sqlStore.executionsList(offset.toInt, length.toInt).map { exec =>
+        val graph = sqlStore.planGraph(exec.executionId)
+        SqlEnrichedData(exec.executionId, graph.allNodes.length, graph.allNodes.map(node => NodePlan(node.id, node.desc)))
+      }
+      val jsonValue: JValue = Extraction.decompose(sqlPlans)(org.json4s.DefaultFormats)
+      jsonValue
     }
-    val jsonValue: JValue = Extraction.decompose(sqlPlans)(org.json4s.DefaultFormats)
-    jsonValue
+    catch {
+      case e: Throwable => {
+        logError("failed to serve dataflint SQL metrics", e)
+        JObject()
+      }
+    }
   }
 
   override def render(request: HttpServletRequest): Seq[Node] = Seq[Node]()
