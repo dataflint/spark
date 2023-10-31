ab286416ed44bee11d616dc571d4a6fca0e54886
OmerElazar <omerelazar269@gmail.com>
2023-10-31 16:18:24 +0200

    Add the alerts to the alerts tab

--- spark-ui/src/tabs/AlertsTab.tsx
+++ spark-ui/src/tabs/AlertsTab.tsx
@@ -1,6 +1,26 @@
+import { Alert, AlertTitle } from "@mui/material";
+import { Stack } from "@mui/system";
 import React from "react";
 import { FC } from "react";
+import { useAppSelector } from "../Hooks";
 
-export const AlertsTab: FC<{}> = ({ }): JSX.Element => {
-    return (<></>);
+export const AlertsTab: FC<{}> = (): JSX.Element => {
+    const alerts = useAppSelector((state) => state.spark.alerts);
+
+    return (
+        <div style={{ overflow: "auto", height: "100%", padding: "15px" }}>
+            <Stack sx={{ width: '100%', whiteSpace: "break-spaces" }} spacing={2}>
+                {
+                    alerts?.alerts.map(alert => {
+                        return <Alert severity={alert.type}>
+                            <AlertTitle>{alert.title}</AlertTitle>
+                            {alert.message}
+                            {"\n"}
+                            {`Suggestions: ${alert.suggestion}`}
+                        </Alert>
+                    })
+                }
+            </Stack>
+        </div>
+    );
 }
