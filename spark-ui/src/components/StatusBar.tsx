6b9491df8e561547aa5279eec0c7b91c9cac5975
menishmueli <menishmueli@gmail.com>
2023-10-31 14:29:21 +0200

    reorg status bar to idle and working bar

--- spark-ui/src/components/StatusBar.tsx
+++ spark-ui/src/components/StatusBar.tsx
@@ -41,103 +41,124 @@ const StatusBar: FC = (): JSX.Element => {
   const numOfQueries = sql?.sqls.length ?? 0;
 
   return (
-    <Grid
-      container
-      spacing={3}
-      sx={{ mt: 2, mb: 2 }}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <InfoBox
-        title={isIdle ? "Idle Time" : "Query Time"}
-        text={isIdle ? idleTimeText : queryRuntimeText}
-        color="#7e57c2"
-        icon={HourglassBottomIcon}
-      ></InfoBox>
-      {currentSql === undefined ||
-        currentSql.stageMetrics === undefined || isIdle ? <InfoBox
-          title="Duration"
-          text={durationText}
-          color="#a31545"
-          icon={AccessTimeIcon}
-        ></InfoBox> : (
-        <InfoBox
-          title="Query Input"
-          text={humanFileSize(currentSql.stageMetrics.inputBytes)}
-          color="#26a69a"
-          icon={ArrowDownwardIcon}
-        ></InfoBox>
-      )}
-      {currentSql === undefined ||
-        currentSql.stageMetrics === undefined || isIdle ? <InfoBox
-          title="Memory Usage"
-          text={status?.executors?.maxExecutorMemoryPercentage.toFixed(2) + "%"}
-          color="#8e24aa"
-          icon={MemoryIcon}
-          tooltipContent={
-            <React.Fragment>
-              <Typography variant="h6" color="inherit" textAlign={"center"}>
-                {status?.executors?.maxExecutorMemoryBytesString} / {executorMemoryBytesString}
-              </Typography>
-              <Typography variant="subtitle2">
-                Peak JVM memory usage / total executor JVM memory
-              </Typography>
-              <Typography variant="subtitle2">
-                "Memory Usage" is the peak memory usage of the most memory-utilized executor.
-              </Typography>
-              <Typography variant="subtitle2">
-                "Executor Memory" refers to the executor JVM memory (both on-heap and off-heap).
-              </Typography>
-            </React.Fragment>
-          }
-        ></InfoBox>
-
-        : (
-          <InfoBox
-            title="Query Output"
-            text={humanFileSize(currentSql.stageMetrics.outputBytes)}
-            color="#ffa726"
-            icon={ArrowUpwardIcon}
-          ></InfoBox>
-        )}
-      <InfoBox
-        title="Executors"
-        text={numOfExecutorsText}
-        color="#52b202"
-        icon={WorkIcon}
-      ></InfoBox>
+    <div>
       {isIdle ?
-        <InfoBox
-          title="SQL Queries"
-          text={numOfQueries.toString()}
-          color='#00838f'
-          icon={DatasetIcon}
-        ></InfoBox>
-        :
-        <InfoBox
-          title="Pending Tasks"
-          text={stagesStatus.totalPendingTasks.toString()}
-          icon={QueueIcon}
-          tooltipContent={
-            <React.Fragment>
-              <Typography variant="h6" color="inherit">
-                What is Pending Tasks?
-              </Typography>
-              <Typography variant="subtitle2">
-                Pending Tasks are the total number of tasks react to be executed
-                on your cluster
-              </Typography>
-              <Typography variant="subtitle2">
-                For example: if you have 3 active stages, each with 1000
-                non-completed tasks, you have 3000 pending tasks
-              </Typography>
-            </React.Fragment>
+        (
+          <Grid
+            container
+            spacing={3}
+            sx={{ mt: 2, mb: 2 }}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <InfoBox
+              title={isIdle ? "Idle Time" : "Query Time"}
+              text={isIdle ? idleTimeText : queryRuntimeText}
+              color="#7e57c2"
+              icon={HourglassBottomIcon}
+            ></InfoBox>
+            <InfoBox
+              title="Duration"
+              text={durationText}
+              color="#a31545"
+              icon={AccessTimeIcon}
+            ></InfoBox>
+            <InfoBox
+              title="Memory Usage"
+              text={status?.executors?.maxExecutorMemoryPercentage.toFixed(2) + "%"}
+              color="#8e24aa"
+              icon={MemoryIcon}
+              tooltipContent={
+                <React.Fragment>
+                  <Typography variant="h6" color="inherit" textAlign={"center"}>
+                    {status?.executors?.maxExecutorMemoryBytesString} / {executorMemoryBytesString}
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    Peak JVM memory usage / total executor JVM memory
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    "Memory Usage" is the peak memory usage of the most memory-utilized executor.
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    "Executor Memory" refers to the executor JVM memory (both on-heap and off-heap).
+                  </Typography>
+                </React.Fragment>
+              }
+            ></InfoBox>
+            <InfoBox
+              title="Executors"
+              text={numOfExecutorsText}
+              color="#52b202"
+              icon={WorkIcon}
+            ></InfoBox>
+            <InfoBox
+              title="SQL Queries"
+              text={numOfQueries.toString()}
+              color='#00838f'
+              icon={DatasetIcon}
+            ></InfoBox>
+          </Grid>) :
+        (<Grid
+          container
+          spacing={3}
+          sx={{ mt: 2, mb: 2 }}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <InfoBox
+            title={isIdle ? "Idle Time" : "Query Time"}
+            text={isIdle ? idleTimeText : queryRuntimeText}
+            color="#7e57c2"
+            icon={HourglassBottomIcon}
+          ></InfoBox>
+          {currentSql === undefined ||
+            currentSql.stageMetrics === undefined ? null : (
+            <InfoBox
+              title="Query Input"
+              text={humanFileSize(currentSql.stageMetrics.inputBytes)}
+              color="#26a69a"
+              icon={ArrowDownwardIcon}
+            ></InfoBox>
+          )}
+          {currentSql === undefined ||
+            currentSql.stageMetrics === undefined ? null :
+            <InfoBox
+              title="Query Output"
+              text={humanFileSize(currentSql.stageMetrics.outputBytes)}
+              color="#ffa726"
+              icon={ArrowUpwardIcon}
+            ></InfoBox>
           }
-        ></InfoBox>}
-
-
-    </Grid>
+          <InfoBox
+            title="Executors"
+            text={numOfExecutorsText}
+            color="#52b202"
+            icon={WorkIcon}
+          ></InfoBox>
+          <InfoBox
+            title="Pending Tasks"
+            text={stagesStatus.totalPendingTasks.toString()}
+            icon={QueueIcon}
+            tooltipContent={
+              <React.Fragment>
+                <Typography variant="h6" color="inherit">
+                  What is Pending Tasks?
+                </Typography>
+                <Typography variant="subtitle2">
+                  Pending Tasks are the total number of tasks react to be executed
+                  on your cluster
+                </Typography>
+                <Typography variant="subtitle2">
+                  For example: if you have 3 active stages, each with 1000
+                  non-completed tasks, you have 3000 pending tasks
+                </Typography>
+              </React.Fragment>
+            }
+          ></InfoBox>
+        </Grid>)}
+    </div>
   );
 };
 
