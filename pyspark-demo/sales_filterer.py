1a2f4ca16374155e15cb93e531fb9821c84d7712
menishmueli <menishmueli@gmail.com>
2023-11-07 18:10:05 +0200

    improve pyspark demo script

--- pyspark-demo/sales_filterer.py
+++ pyspark-demo/sales_filterer.py
@@ -1,15 +1,20 @@
+#!/usr/bin/python3
 from pyspark.sql import SparkSession
+import os
 
 spark = SparkSession \
     .builder \
     .appName("Sales Filterer") \
+    .config("spark.jars.packages", "io.dataflint:spark_2.12:0.0.1-SNAPSHOT") \
+    .config("spark.jars.repositories", "https://s01.oss.sonatype.org/content/repositories/snapshots") \
+    .config("spark.plugins", "io.dataflint.spark.SparkDataflintPlugin") \
     .config("spark.ui.port", "11000") \
     .config("spark.eventLog.enabled", True) \
     .config("spark.sql.maxMetadataStringLength", "10000") \
     .master("local[1]") \
     .getOrCreate()
 
-df = spark.read.load("/Users/menishmueli/Documents/GitHub/spark-sql-perf/data/store_sales")
+df = spark.read.load(os.getenv('SALES_FILES_LOCATION'))
 
 df_filtered = df.filter(df.ss_quantity > 1)
 
@@ -18,6 +23,5 @@ df_filtered.write \
     .partitionBy("ss_quantity") \
     .parquet("/tmp/store_sales")
 
-# input("job ended")
-
+input("sales filterer ended, press any key to continue..")
 spark.stop()
