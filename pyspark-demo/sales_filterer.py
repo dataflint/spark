10fd33684eadfabbe9eb5d81b3a1afd48bc8a53f
menishmueli <menishmueli@gmail.com>
2023-11-13 10:29:32 +0200

    version bump to 0.0.2

--- pyspark-demo/sales_filterer.py
+++ pyspark-demo/sales_filterer.py
@@ -5,7 +5,7 @@ import os
 spark = SparkSession \
     .builder \
     .appName("Sales Filterer") \
-    .config("spark.jars.packages", "io.dataflint:spark_2.12:0.0.1-SNAPSHOT") \
+    .config("spark.jars.packages", "io.dataflint:spark_2.12:0.0.2-SNAPSHOT") \
     .config("spark.jars.repositories", "https://s01.oss.sonatype.org/content/repositories/snapshots") \
     .config("spark.plugins", "io.dataflint.spark.SparkDataflintPlugin") \
     .config("spark.ui.port", "11000") \
--- pyspark-demo/submit_sales_filterer.sh
+++ pyspark-demo/submit_sales_filterer.sh
@@ -1,5 +1,5 @@
 spark-submit \
---packages io.dataflint:spark_2.12:0.0.1-SNAPSHOT \
+--packages io.dataflint:spark_2.12:0.0.2-SNAPSHOT \
 --repositories https://s01.oss.sonatype.org/content/repositories/snapshots \
 --conf spark.plugins=io.dataflint.spark.SparkDataflintPlugin \
 /Users/menishmueli/Documents/GitHub/dataflint/spark/pyspark-demo/sales_filterer.py
