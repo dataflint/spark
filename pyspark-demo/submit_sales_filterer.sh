ca3f2f9783d61d55670b4f7528e8342751a3500d
menishmueli <menishmueli@gmail.com>
2023-11-16 15:37:49 +0200

    version bump to 0.0.4

--- pyspark-demo/submit_sales_filterer.sh
+++ pyspark-demo/submit_sales_filterer.sh
@@ -1,5 +1,5 @@
 spark-submit \
---packages io.dataflint:spark_2.12:0.0.3-SNAPSHOT \
+--packages io.dataflint:spark_2.12:0.0.4-SNAPSHOT \
 --repositories https://s01.oss.sonatype.org/content/repositories/snapshots \
 --conf spark.plugins=io.dataflint.spark.SparkDataflintPlugin \
 /Users/menishmueli/Documents/GitHub/dataflint/spark/pyspark-demo/sales_filterer.py
