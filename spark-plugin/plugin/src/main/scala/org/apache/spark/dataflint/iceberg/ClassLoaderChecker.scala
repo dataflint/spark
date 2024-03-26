91ba1e0cfb6790a0de9539a76ba562684a6cf56b
menishmueli <menishmueli@gmail.com>
2024-03-26 11:53:44 +0200

    add additional check for NoClassDefFoundError error in iceberg metric reporter

--- spark-plugin/plugin/src/main/scala/org/apache/spark/dataflint/iceberg/ClassLoaderChecker.scala
+++ spark-plugin/plugin/src/main/scala/org/apache/spark/dataflint/iceberg/ClassLoaderChecker.scala
@@ -5,19 +5,25 @@ import org.apache.spark.internal.Logging
 
 object ClassLoaderChecker extends Logging {
   def isMetricLoaderInRightClassLoader(): Boolean = {
-    val metricReporterClass = classOf[DataflintIcebergMetricsReporter]
-    val classLoaderMetricReporter = metricReporterClass.getClassLoader.toString
-    val classLoaderIcebergCatalog = classOf[CatalogUtil].getClassLoader.toString
     try {
-      Class.forName(metricReporterClass.getCanonicalName, false, classOf[CatalogUtil].getClassLoader)
-    } catch {
-      case _: ClassNotFoundException =>
-        logWarning(s"Cannot load DataflintIcebergMetricsReporter from iceberg classloader, which prevents dataflint iceberg observability support. iceberg classloader: ${classLoaderIcebergCatalog} metric reporter classloader: ${classLoaderMetricReporter}")
-        return false
-      case error: Throwable =>
-        logError(s"Unexpected error while trying to load, can use DataflintIcebergMetricsReporter. iceberg classloader: ${classLoaderIcebergCatalog} metric reporter classloader: ${classLoaderMetricReporter}", error)
-        return false
+      val metricReporterClass = classOf[DataflintIcebergMetricsReporter]
+      val classLoaderMetricReporter = metricReporterClass.getClassLoader.toString
+      val classLoaderIcebergCatalog = classOf[CatalogUtil].getClassLoader.toString
+      try {
+        Class.forName(metricReporterClass.getCanonicalName, false, classOf[CatalogUtil].getClassLoader)
+      }
+      catch {
+        case _: NoClassDefFoundError =>
+          logWarning(s"Cannot load iceberg MetricsReporter from dataflint classloader, which prevents dataflint iceberg observability support. iceberg classloader: ${classOf[CatalogUtil].getClassLoader.toString}")
+          return false
+        case _: ClassNotFoundException =>
+          logWarning(s"Cannot load DataflintIcebergMetricsReporter from iceberg classloader, which prevents dataflint iceberg observability support. iceberg classloader: ${classLoaderIcebergCatalog} metric reporter classloader: ${classLoaderMetricReporter}")
+          return false
+        case error: Throwable =>
+          logError(s"Unexpected error while trying to load, can use DataflintIcebergMetricsReporter. iceberg classloader: ${classLoaderIcebergCatalog} metric reporter classloader: ${classLoaderMetricReporter}", error)
+          return false
+      }
+      true
     }
-    true
   }
 }
