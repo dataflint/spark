f6ff63452c7d19d87c2ae8ff5955e301c4ce9db0
menishmueli <menishmueli@gmail.com>
2024-04-30 18:24:13 +0300

    fix quantiles collection for SaaS

--- spark-plugin/plugin/src/main/scala/org/apache/spark/dataflint/saas/StoreDataExtractor.scala
+++ spark-plugin/plugin/src/main/scala/org/apache/spark/dataflint/saas/StoreDataExtractor.scala
@@ -34,7 +34,7 @@ class StoreDataExtractor(store: AppStatusStore) {
   }
 
   private def calculateTaskSummary(): Seq[StageTaskSummary] = {
-    val quantiles = Array(0.0, 0.01, 0.05, 0.1, 0, 2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.75, 0.8, 0.9, 0.95, 0.99, 1.0)
+    val quantiles = Array(0.0, 0.01, 0.05, 0.1, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.75, 0.8, 0.9, 0.95, 0.99, 1.0)
     store.stageList(null).map(stage => {
       store.taskSummary(stage.stageId, stage.attemptId, quantiles).map(
         StageTaskSummary(stage.stageId, stage.attemptId, _)
