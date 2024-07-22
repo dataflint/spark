0528ab225eaa625c06d1c49daff1222a0206612f
menishmueli <menishmueli@gmail.com>
2024-07-22 11:32:07 +0300

    fixes for ExecutorsMetricsSerializer

--- spark-plugin/plugin/src/main/scala/org/apache/spark/dataflint/saas/ExecutorsMetricsSerializer.scala
+++ spark-plugin/plugin/src/main/scala/org/apache/spark/dataflint/saas/ExecutorsMetricsSerializer.scala
@@ -2,23 +2,23 @@ package org.apache.spark.dataflint.saas
 
 import org.apache.spark.executor.ExecutorMetrics
 import org.apache.spark.metrics.ExecutorMetricType
-import org.json4s.{CustomSerializer, JInt, JNull, JObject}
+import org.json4s.{CustomSerializer, JLong, JNull, JObject}
 import org.json4s.JsonAST.JValue
 
-class ExecutorsMetricsSerializer extends CustomSerializer[Option[ExecutorMetrics]](implicit format => (
+class ExecutorsMetricsSerializer extends CustomSerializer[ExecutorMetrics](implicit format => (
   {
-    case JNull => None
     case json: JValue =>
       val metricsMap = json.extract[Map[String, Long]]
       val metrics =  new ExecutorMetrics(metricsMap)
-      Some(metrics)
+      metrics
   },
   {
     case Some(metrics: ExecutorMetrics) =>
       val metricsMap = ExecutorMetricType.metricToOffset.map { case (metric, _) =>
         metric -> metrics.getMetricValue(metric)
       }
-      JObject(metricsMap.map { case (k, v) => k -> JInt(v) }.toList)
+      val metricsObj = JObject(metricsMap.map { case (k, v) => k -> JLong(v) }.toList)
+      metricsObj
     case None => JNull
   }
 ))
