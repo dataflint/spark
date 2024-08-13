84564418d14f70e9a88eb71016ad1b5059361e3a
menishmueli <menishmueli@gmail.com>
2024-08-13 16:10:07 +0300

    improve large broadcast example

--- spark-plugin/example_3_5_1/src/main/scala/io/dataflint/example/LargeBroadcastExample.scala
+++ spark-plugin/example_3_5_1/src/main/scala/io/dataflint/example/LargeBroadcastExample.scala
@@ -1,11 +1,10 @@
 package main.scala.io.dataflint.example
 
+import io.dataflint.example.LargeFilterCondition.spark
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object LargeBroadcastExample extends App {
-
-
   val spark = SparkSession
     .builder()
     .appName("LargeBroadcastExample")
@@ -14,21 +13,26 @@ object LargeBroadcastExample extends App {
     .config("spark.ui.port", "10000")
     .config("spark.dataflint.telemetry.enabled", value = false)
     .config("spark.sql.maxMetadataStringLength", "10000")
-    .config("spark.driver.memory", "8g")
-    .config("spark.eventLog.enabled", "true")
-    .config("spark.eventLog.dir", "/opt/spark/conf/spark-events")
+    .config("spark.driver.maxResultSize", "10g")
     .master("local[*]")
     .getOrCreate()
-  
-  val numberRows = sys.env.get("NUMBER_OF_ROWS").map(_.toInt).getOrElse(100000)
 
-  val largeDF = spark.range(1, 5000000).toDF("item_sk")
+  spark.sparkContext.setJobDescription("Join with large broadcast")
 
-  val smallDF = spark.range(1, numberRows).toDF("id")
+  val smallDfSize = sys.env.get("SMALL_DF_SIZE").map(_.toLong).getOrElse((40 * 1000 * 1000).toLong)
+  val largeDFSize = sys.env.get("LARGE_DF_SIZE").map(_.toLong).getOrElse((100 * 1000 * 1000).toLong)
+
+  val smallDF = spark.range(1L, smallDfSize).toDF("id")
+  val largeDF = spark.range(1L, largeDFSize).toDF("item_sk")
 
   val joinedDF = largeDF.join(broadcast(smallDF), largeDF("item_sk") === smallDF("id"))
 
-  joinedDF.show()
+  joinedDF.count()
+
+  spark.sparkContext.setJobDescription("Join with shuffle")
+  val joinedWithShuffleDF = largeDF.join(smallDF, largeDF("item_sk") === smallDF("id"))
+
+  joinedWithShuffleDF.count()
 
   scala.io.StdIn.readLine()
   spark.stop()
