eec6a45a70698ae33d5a7e8ca04cce94ae69bbbc
snstrauss <zahavi.guy.85@gmail.com>
2025-03-12 17:35:41 +0200

    moved SqlLayoutService into it's own folder, and it is now using the external builders for clarity. added method for getting grouped layout

---
+++ spark-ui/src/components/SqlFlow/SqlLayoutService/SqlLayoutService.ts
@@ -0,0 +1,48 @@
+import { Edge, Node } from "reactflow";
+import { EnrichedSparkSQL, GraphFilter } from "../../../interfaces/AppStore";
+import { getFlatElementsLayout } from "./dagreLayouts";
+import {
+  getFlowNodes,
+  getGroupNodes,
+  toFlowEdge,
+  transformEdgesToGroupEdges,
+} from "./layoutServiceBuilders";
+
+class SqlLayoutService {
+  static SqlElementsToLayout(
+    sql: EnrichedSparkSQL,
+    graphFilter: GraphFilter,
+  ): { layoutNodes: Node[]; layoutEdges: Edge[] } {
+    const { edges } = sql.filters[graphFilter];
+
+    const flowNodes = getFlowNodes(sql, graphFilter);
+    const flowEdges = edges.map(toFlowEdge);
+
+    const { layoutNodes, layoutEdges } = getFlatElementsLayout(
+      flowNodes,
+      flowEdges,
+    );
+    return { layoutNodes: layoutNodes, layoutEdges: layoutEdges };
+  }
+  static SqlElementsToGroupedLayout(
+    sql: EnrichedSparkSQL,
+    graphFilter: GraphFilter,
+  ): { layoutNodes: Node[]; layoutEdges: Edge[] | [] } {
+    const { edges } = sql.filters[graphFilter];
+
+    const flowNodes = getFlowNodes(sql, graphFilter);
+    const flowGroupNodes = getGroupNodes(flowNodes);
+    const nodeAndGroupEdges = transformEdgesToGroupEdges(
+      flowNodes,
+      flowGroupNodes,
+      edges,
+    );
+
+    return {
+      layoutNodes: [...flowNodes, ...flowGroupNodes],
+      layoutEdges: nodeAndGroupEdges as Edge[],
+    };
+  }
+}
+
+export default SqlLayoutService;
