12d60c7a9adf5883a369d2cab262fb21f4fc11f8
snstrauss <zahavi.guy.85@gmail.com>
2025-03-12 17:34:36 +0200

    moved dagre layout helper into it's own file, as preparation for addition of grouped layout helper

---
+++ spark-ui/src/components/SqlFlow/SqlLayoutService/dagreLayouts.ts
@@ -0,0 +1,41 @@
+import dagre from "dagre";
+import { Edge, Node, Position } from "reactflow";
+
+const nodeWidth = 280;
+const nodeHeight = 280;
+
+export const getFlatElementsLayout = (
+  nodes: Node[],
+  edges: Edge[],
+): { layoutNodes: Node[]; layoutEdges: Edge[] } => {
+  const dagreGraph = new dagre.graphlib.Graph();
+  dagreGraph.setDefaultEdgeLabel(() => ({}));
+  dagreGraph.setGraph({ rankdir: "LR" });
+
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  nodes.forEach((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    node.targetPosition = Position.Left;
+    node.sourcePosition = Position.Right;
+
+    // We are shifting the dagre node position (anchor=center center) to the top left
+    // so it matches the React Flow node anchor point (top left).
+    node.position = {
+      x: nodeWithPosition.x - nodeWidth / 2,
+      y: nodeWithPosition.y - nodeHeight / 2,
+    };
+
+    return node;
+  });
+
+  return { layoutNodes: nodes, layoutEdges: edges };
+};
