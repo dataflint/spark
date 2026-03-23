6491fb2fcf465b7ec5975f3baf8026d30e58a297
Avi Minsky <minsky.a@gmail.com>
2026-03-23 16:10:47 +0200

    added readln

--- spark-plugin/pyspark-testing/dataflint_pyspark_example.py
+++ spark-plugin/pyspark-testing/dataflint_pyspark_example.py
@@ -371,3 +371,6 @@ print("\nResult written to /tmp/dataflint_flat_map_cogroups_in_pandas_example")
 print("\n" + "="*80)
 print("Done!")
 print("="*80)
+
+input("\nPress Enter to exit...")
+spark.stop()
