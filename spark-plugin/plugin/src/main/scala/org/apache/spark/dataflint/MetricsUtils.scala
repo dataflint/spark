c915d8925103462a0e422fd3b7a9e2c5f8da0efa
Avi Minsky <minsky.a@gmail.com>
2026-03-25 16:03:18 +0200

    public api and only then old way

--- spark-plugin/plugin/src/main/scala/org/apache/spark/dataflint/MetricsUtils.scala
+++ spark-plugin/plugin/src/main/scala/org/apache/spark/dataflint/MetricsUtils.scala
@@ -1,14 +1,19 @@
 package org.apache.spark.dataflint
 
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.execution.metric.SQLMetric
+import org.apache.spark.sql.execution.metric.{SQLMetric, SQLMetrics}
 
 object MetricsUtils {
-  def getTimingMetric(name: String)(implicit sparkContext:SparkContext): (String, SQLMetric) = {
+  def getTimingMetric(name: String)(implicit sparkContext: SparkContext): (String, SQLMetric) = {
     name -> {
-      val metric = new SQLMetric("timing", -1L)
-      metric.register(sparkContext, Some(name), countFailedValues = false)
-      metric
+      try {
+        SQLMetrics.createTimingMetric(sparkContext, name)
+      } catch {
+        case _: NoSuchMethodError =>
+          val metric = new SQLMetric("timing", -1L)
+          metric.register(sparkContext, Some(name), countFailedValues = false)
+          metric
+      }
     }
   }
 }
